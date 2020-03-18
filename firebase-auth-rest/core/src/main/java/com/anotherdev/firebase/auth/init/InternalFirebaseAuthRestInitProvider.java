4b5c294429d59ccc06835e6ba88536e7905fff94
anotherdev <polson.k+github@gmail.com>
2020-03-18 14:16:28 +0700

    Init timber. Try access google_api_key string

--- firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/init/InternalFirebaseAuthRestInitProvider.java
+++ firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/init/InternalFirebaseAuthRestInitProvider.java
@@ -1,11 +1,28 @@
 package com.anotherdev.firebase.auth.init;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+
 import com.anotherdev.firebase.auth.core.BaseInitProvider;
+import com.anotherdev.firebase.auth.core.R;
+
+import timber.log.Timber;
 
 public class InternalFirebaseAuthRestInitProvider extends BaseInitProvider {
 
+    @SuppressLint("LogNotTimber")
     @Override
     public boolean onCreate() {
-        return super.onCreate();
+        try {
+            Timber.plant(new Timber.DebugTree());
+            Context appContext = getAppContext();
+            String googleApiKey = appContext.getString(R.string.google_api_key);
+
+            return true;
+        } catch (Exception e) {
+            Log.e(InternalFirebaseAuthRestInitProvider.class.getSimpleName(), e.getMessage(), e);
+            return false;
+        }
     }
 }
