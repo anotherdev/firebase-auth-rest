4b91528dfc53ea06e1e79715862bf26a81b801aa
anotherdev <polson.k+github@gmail.com>
2020-03-19 02:03:33 +0700

    initial auth provider

--- firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/FirebaseAuthRest.java
+++ firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/FirebaseAuthRest.java
@@ -1,4 +1,18 @@
 package com.anotherdev.firebase.auth;
 
+import com.anotherdev.firebase.auth.common.FirebaseAuth;
+import com.anotherdev.firebase.auth.rest.RestAuthProvider;
+import com.google.firebase.FirebaseApp;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class FirebaseAuthRest {
+
+    private static final Map<String, FirebaseAuth> APP_AUTH_PROVIDER_MAP = new ConcurrentHashMap<>();
+
+
+    public static FirebaseAuth getInstance(FirebaseApp app) {
+        return new RestAuthProvider(app);
+    }
 }
--- firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/rest/RestAuthProvider.java
+++ firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/rest/RestAuthProvider.java
@@ -1,4 +1,44 @@
 package com.anotherdev.firebase.auth.rest;
 
-public class RestAuthProvider {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.anotherdev.firebase.auth.common.FirebaseAuth;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.auth.internal.IdTokenListener;
+
+public class RestAuthProvider implements FirebaseAuth {
+
+    private final FirebaseApp app;
+
+
+    public RestAuthProvider(FirebaseApp app) {
+        this.app = app;
+    }
+
+    @Override
+    public void signInAnonymously() {
+    }
+
+    @NonNull
+    @Override
+    public Task<GetTokenResult> getAccessToken(boolean forceRefresh) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getUid() {
+        return null;
+    }
+
+    @Override
+    public void addIdTokenListener(@NonNull IdTokenListener idTokenListener) {
+    }
+
+    @Override
+    public void removeIdTokenListener(@NonNull IdTokenListener idTokenListener) {
+    }
 }
