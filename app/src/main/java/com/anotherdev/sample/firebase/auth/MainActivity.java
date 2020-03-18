1516f68177ad38069f9a3b3628cb14d953923a09
anotherdev <polson.k+github@gmail.com>
2020-03-19 02:11:38 +0700

    keep screen on programmatically

--- app/src/main/java/com/anotherdev/sample/firebase/auth/MainActivity.java
+++ app/src/main/java/com/anotherdev/sample/firebase/auth/MainActivity.java
@@ -1,14 +1,25 @@
 package com.anotherdev.sample.firebase.auth;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.anotherdev.firebase.auth.FirebaseAuthRest;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
         setContentView(R.layout.activity_main);
     }
 }
