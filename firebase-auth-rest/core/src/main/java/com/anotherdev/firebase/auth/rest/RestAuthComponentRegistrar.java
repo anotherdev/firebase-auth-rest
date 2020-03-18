bac1eadf60f09c6b4b9b9251c4d4ccdb9dbef855
anotherdev <polson.k+github@gmail.com>
2020-03-18 17:48:12 +0700

    add initial RestAuthComponentRegistrar

---
+++ firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/rest/RestAuthComponentRegistrar.java
@@ -0,0 +1,15 @@
+package com.anotherdev.firebase.auth.rest;
+
+import com.google.firebase.components.Component;
+import com.google.firebase.components.ComponentRegistrar;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RestAuthComponentRegistrar implements ComponentRegistrar {
+
+    @Override
+    public List<Component<?>> getComponents() {
+        return Collections.emptyList();
+    }
+}
