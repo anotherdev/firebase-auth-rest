844c58083e5882eb3adf069a895b466e2898a8ab
anotherdev <polson.k+github@gmail.com>
2020-03-18 20:45:52 +0700

    add debug log

--- firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/rest/RestAuthComponentRegistrar.java
+++ firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/rest/RestAuthComponentRegistrar.java
@@ -10,14 +10,21 @@ import com.google.firebase.components.Dependency;
 import java.util.Collections;
 import java.util.List;
 
+import timber.log.Timber;
+
 public class RestAuthComponentRegistrar implements ComponentRegistrar {
 
     @Override
     public List<Component<?>> getComponents() {
-        Component<InternalAuthProvider> component = Component.builder(InternalAuthProvider.class)
-                .add(Dependency.required(FirebaseApp.class))
+        Timber.i("Called getComponents()");
+        final Class<InternalAuthProvider> authProviderClass = InternalAuthProvider.class;
+        final Class<FirebaseApp> firebaseAppClass = FirebaseApp.class;
+
+        Component<InternalAuthProvider> component = Component.builder(authProviderClass)
+                .add(Dependency.required(firebaseAppClass))
                 .factory(container -> {
-                    FirebaseApp app = container.get(FirebaseApp.class);
+                    FirebaseApp app = container.get(firebaseAppClass);
+                    Timber.i("create auth provider: %s for app: %s", authProviderClass, app);
                     return FirebaseAuthRest.getInstance(app);
                 })
                 .build();
