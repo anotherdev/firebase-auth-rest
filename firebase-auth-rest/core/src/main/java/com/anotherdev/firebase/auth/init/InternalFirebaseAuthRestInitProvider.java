661243103bc75be0ee789f9c51dc202ed5c090e3
anotherdev <polson.k+github@gmail.com>
2020-03-18 13:33:43 +0700

    add firebase-auth-rest init provider

---
+++ firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/init/InternalFirebaseAuthRestInitProvider.java
@@ -0,0 +1,11 @@
+package com.anotherdev.firebase.auth.init;
+
+import com.anotherdev.firebase.auth.core.BaseInitProvider;
+
+public class InternalFirebaseAuthRestInitProvider extends BaseInitProvider {
+
+    @Override
+    public boolean onCreate() {
+        return super.onCreate();
+    }
+}
