255b353fcb2d1b7fd2aeb292a2cd6b85712a80a6
anotherdev <polson.k+github@gmail.com>
2020-03-19 13:18:28 +0700

    test app

--- app/src/main/java/com/anotherdev/sample/firebase/auth/MainActivity.java
+++ app/src/main/java/com/anotherdev/sample/firebase/auth/MainActivity.java
@@ -7,10 +7,12 @@ import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.anotherdev.firebase.auth.FirebaseAuthRest;
+import com.anotherdev.firebase.auth.FirebaseUser;
+import com.anotherdev.firebase.auth.rest.api.model.SignInAnonymouslyResponse;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+
+import io.reactivex.rxjava3.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,38 @@ public class MainActivity extends AppCompatActivity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
         setContentView(R.layout.activity_main);
+
+        FirebaseApp app = FirebaseApp.getInstance();
+        Log.e(getClass().getSimpleName(), String.valueOf(app));
+
+        com.anotherdev.firebase.auth.common.FirebaseAuth auth = FirebaseAuthRest.getInstance(app);
+        com.anotherdev.firebase.auth.FirebaseUser user = auth.getCurrentUser();
+
+        Log.e("FAR", "auth.getUid(): " + auth.getUid());
+        Log.e("FAR", "user: " + user);
+
+        if (FirebaseUser.SIGNED_OUT.equals(user)) {
+            auth.signInAnonymously()
+                    .doOnError(new Consumer<Throwable>() {
+                        @Override
+                        public void accept(Throwable e) throws Throwable {
+                            Log.e("FAR", "error", e);
+                        }
+                    })
+                    .onErrorComplete()
+                    .doOnSuccess(new Consumer<SignInAnonymouslyResponse>() {
+                        @Override
+                        public void accept(SignInAnonymouslyResponse response) throws Throwable {
+                            Log.e("FAR", "doOnSuccess: " + auth.getCurrentUser());
+                        }
+                    })
+                    .subscribe();
+        } else {
+            Log.e("FAR", "Already signed in: " + user);
+        }
+
+        FirebaseDatabase.getInstance()
+                .getReference("test")
+                .setValue(System.currentTimeMillis());
     }
 }
