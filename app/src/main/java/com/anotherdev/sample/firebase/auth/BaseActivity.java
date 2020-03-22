934f8a29dd417cbaf29fe06aac6e840ece2dd711
anotherdev <polson.k+github@gmail.com>
2020-03-22 17:26:29 +0700

    add new activities

---
+++ app/src/main/java/com/anotherdev/sample/firebase/auth/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.anotherdev.sample.firebase.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/anotherdev/sample/firebase/auth/StartActivity.java
@@ -0,0 +1,14 @@
+package com.anotherdev.sample.firebase.auth;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
