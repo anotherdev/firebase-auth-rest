22005e596c63e42e23a3089a91d94c4cdf2a340f
anotherdev <polson.k+github@gmail.com>
2020-03-22 22:09:25 +0700

    add @CheckResultValue annotation

--- firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/common/FirebaseAuth.java
+++ firebase-auth-rest/core/src/main/java/com/anotherdev/firebase/auth/common/FirebaseAuth.java
@@ -6,6 +6,7 @@ import com.anotherdev.firebase.auth.FirebaseUser;
 import com.anotherdev.firebase.auth.rest.api.model.SignInAnonymouslyResponse;
 import com.google.firebase.auth.internal.InternalAuthProvider;
 
+import io.reactivex.rxjava3.annotations.CheckReturnValue;
 import io.reactivex.rxjava3.core.Single;
 
 public interface FirebaseAuth extends InternalAuthProvider {
@@ -13,6 +14,7 @@ public interface FirebaseAuth extends InternalAuthProvider {
     @Nullable
     FirebaseUser getCurrentUser();
 
+    @CheckReturnValue
     Single<SignInAnonymouslyResponse> signInAnonymously();
 
     void signOut();
