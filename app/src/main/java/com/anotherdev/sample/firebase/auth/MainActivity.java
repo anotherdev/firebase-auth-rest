ed92e73cafecb488d5d5cdb2d286d4fc549c12f6
anotherdev <polson.k+github@gmail.com>
2020-03-23 14:21:46 +0700

    add account menu

--- app/src/main/java/com/anotherdev/sample/firebase/auth/MainActivity.java
+++ app/src/main/java/com/anotherdev/sample/firebase/auth/MainActivity.java
@@ -2,8 +2,13 @@ package com.anotherdev.sample.firebase.auth;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 
 import com.anotherdev.firebase.auth.FirebaseAuthRest;
+import com.anotherdev.sample.firebase.auth.intent.LoginIntent;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -51,5 +56,21 @@ public class MainActivity extends BaseActivity {
         FirebaseDatabase.getInstance()
                 .getReference("test")
                 .setValue(System.currentTimeMillis());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        final int itemId = item.getItemId();
+        if (R.id.action_view_accounts == itemId) {
+            startActivity(new LoginIntent(this));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
