33aaa61b82d020ab4836b4538daee037ee28a2c9
anotherdev <polson.k+github@gmail.com>
2020-04-06 14:20:49 +0700

    try dismiss gms dialog

--- app/src/main/java/com/anotherdev/sample/firebase/auth/BaseActivity.java
+++ app/src/main/java/com/anotherdev/sample/firebase/auth/BaseActivity.java
@@ -1,6 +1,8 @@
 package com.anotherdev.sample.firebase.auth;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.annotation.CallSuper;
@@ -8,6 +10,8 @@ import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.anotherdev.firebase.auth.FirebaseAuthRest;
 import com.anotherdev.firebase.auth.FirebaseUser;
@@ -15,15 +19,23 @@ import com.anotherdev.firebase.auth.common.FirebaseAuth;
 import com.anotherdev.firebase.auth.util.RxUtil;
 import com.anotherdev.sample.firebase.auth.intent.LoginIntent;
 import com.github.florent37.inlineactivityresult.rx.RxInlineActivityResult;
+import com.google.android.gms.common.SupportErrorDialogFragment;
 import com.google.firebase.FirebaseApp;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import hu.akarnokd.rxjava3.bridge.RxJavaBridge;
 import io.reactivex.internal.functions.Functions;
+import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    protected final String TAG = getClass().getSimpleName();
 
     protected final CompositeDisposable onDestroy = new CompositeDisposable();
 
@@ -44,6 +56,7 @@ public abstract class BaseActivity extends AppCompatActivity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
         setContentView();
+        onDestroy.add(hideGmsDialog());
     }
 
     protected Toolbar getToolbar() {
@@ -64,6 +77,44 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected boolean isLayoutProvided() {
         return getActivityLayoutRes() != 0;
+    }
+
+    private Disposable hideGmsDialog() {
+        Window window = getWindow();
+        if (window != null) {
+            window.getDecorView()
+                    .getViewTreeObserver()
+                    .addOnWindowFocusChangeListener(hasFocus -> {
+                        Log.d(TAG, String.format("onWindowFocusChanged: hasFocus %s, isFinishing: %s", hasFocus, isFinishing()));
+                        if (!hasFocus && !isFinishing()) {
+                            findAndDismissGmsDialog();
+                        }
+                    });
+
+            return Flowable.interval(25, TimeUnit.MILLISECONDS)
+                    .take(10)
+                    .doOnNext(tick -> findAndDismissGmsDialog())
+                    .subscribe(io.reactivex.rxjava3.internal.functions.Functions.emptyConsumer(), RxUtil.ON_ERROR_LOG_V3);
+        } else {
+            return Flowable.empty().subscribe();
+        }
+    }
+
+    private boolean findAndDismissGmsDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        List<Fragment> fragments = fm.getFragments();
+        Log.d(TAG, String.format("Found %s fragments: %s", fragments.size(), fragments));
+        for (Fragment f : fragments) {
+            String fragmentClassName = f.getClass().getName();
+            Log.d(TAG, String.format("Fragment class: %s obj: %s", fragmentClassName, f));
+            if (f instanceof SupportErrorDialogFragment && "GooglePlayServicesErrorDialog".equals(f.getTag())) {
+                SupportErrorDialogFragment d = (SupportErrorDialogFragment) f;
+                d.dismiss();
+                Log.w(TAG, String.format("Dismissed fragment: %s", fragmentClassName));
+                return true;
+            }
+        }
+        return false;
     }
 
     protected void requestAuthIfNeeded() {
