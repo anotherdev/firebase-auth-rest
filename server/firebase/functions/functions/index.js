247485d57ac96b6831c19e4b68cfeb0938996b7f
anotherdev <polson.k+github@gmail.com>
2020-05-18 16:47:28 +0700

    init Firebase Admin SDK

--- server/firebase/functions/functions/index.js
+++ server/firebase/functions/functions/index.js
@@ -6,3 +6,7 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
