308b7b5a13b6d06f20fe802b34e9029fa07b1d22
anotherdev <polson.k+github@gmail.com>
2020-05-22 16:57:51 +0700

    setup webview

--- app/src/main/java/com/anotherdev/sample/firebase/auth/JsPhoneLoginActivity.java
+++ app/src/main/java/com/anotherdev/sample/firebase/auth/JsPhoneLoginActivity.java
@@ -1,7 +1,14 @@
 package com.anotherdev.sample.firebase.auth;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.Nullable;
 
@@ -16,9 +23,40 @@ public class JsPhoneLoginActivity extends BaseActivity {
         return R.layout.activity_js_phone_login;
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("JavaScript Phone Login");
+
+        webView.setWebViewClient(webViewClient);
+        webView.setWebChromeClient(webChromeClient);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        CookieManager.getInstance().setAcceptCookie(true);
+
         webView.loadUrl("file:///android_asset/phone_login.html");
     }
+
+
+    private final WebViewClient webViewClient = new WebViewClient() {
+    };
+
+
+    private final WebChromeClient webChromeClient = new WebChromeClient() {
+
+        @Override
+        public boolean onConsoleMessage(ConsoleMessage msg) {
+            final String className = getClass().getName();
+            final int line = msg.lineNumber();
+            final ConsoleMessage.MessageLevel level = msg.messageLevel();
+            final String message = msg.message();
+            final String sourceId = msg.sourceId();
+            String error = String.format("class: %s level: %s line: %s msg: %s sourceId: %s", className, level, line, message, sourceId);
+            Log.e(TAG, error);
+            return false;
+        }
+    };
 }
