98a6bbd1a0b338f3db81212eb02990df42668237
anotherdev <polson.k+github@gmail.com>
2020-05-24 14:46:19 +0700

    remove unused code

--- app/src/main/assets/init-firebase.js
+++ app/src/main/assets/init-firebase.js
@@ -15,20 +15,10 @@ firebase.initializeApp(firebaseConfig);
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    console.log("SIGNED IN: " + uid + " " + email + " " + user.refreshToken);
+    console.log("SIGNED IN: " + user);
+    var json = JSON.stringify(user);
+    FirebaseAuthJs.onAuthStateChanged(json);
   } else {
     console.log("SIGNED OUT");
-    firebase.auth().signInWithEmailAndPassword("a@b.com", "123456").catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode + errorMessage);
-    });
   }
 });
